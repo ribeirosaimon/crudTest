3582f669e458eb76c835f75c092ccd5a20adb51a
Saimon Ribeiro <saimonribeiros@hotmail.com>
2022-03-14 14:54:02 -0300

    init

---
+++ src/main/java/com/saimon/tuBank/TuBankApplication.java
@@ -0,0 +1,13 @@
+package com.saimon.tuBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuBankApplication.class, args);
+	}
+
+}
