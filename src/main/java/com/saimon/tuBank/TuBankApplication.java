43e44e186d65776846bf28ea88f521c3ddad68b2
Saimon Ribeiro <saimonribeiros@hotmail.com>
2022-03-21 12:30:45 -0300

    change user to client

--- src/main/java/com/saimon/tuBank/TuBankApplication.java
+++ src/main/java/com/saimon/tuBank/TuBankApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TuBankApplication {
 
 	public static void main(String[] args) {
